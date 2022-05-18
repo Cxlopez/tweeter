a04395c0e22d830f68b6eb2cdc4acda8aae29ee5
Cxlopez <cristian4402@hotmail.com>
2022-05-18 11:26:11 -0400

    using jquery function to make the .counter decrease for each letter entered in word box, changing to red if over limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+$(document).ready(function() {
+  $('#tweet-text').keyup(function() {
+    const maxChar = 140;
+    const typedChars = $(this).val();
+    const charsLeft = maxChar - typedChars.length;
+    $('.counter').text(charsLeft);
+    if (charsLeft < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'black');
+    }
+  });
+});
