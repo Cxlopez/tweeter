3fe856b183dd92c858289327c9d49e5ed97aa69f
Cxlopez <cristian4402@hotmail.com>
2022-05-26 00:30:58 -0400

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,36 +5,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+  $('.tweet-container').html('');
+
   for (const tweet of tweets) {
     const tweetVal = createTweetElement(tweet);
     $('.tweet-container').append(tweetVal);
@@ -52,7 +29,7 @@ const createTweetElement = (tweet) => {
   </header>
   <p class="tweet-message">${tweet.content.text}</p>
   <footer>
-    <p class="tweet-date">${tweet.created_at}</p>
+    <p class="tweet-date">${timeago.format(tweet.created_at)}</p>
     <div class="react-icons">
       <i class="flag fa-solid fa-flag"></i>
       <i class="retweet fa-solid fa-retweet"></i>
@@ -68,15 +45,31 @@ $(document).ready(function() {
     event.preventDefault();
     const form = $(this);
     const text = form.serialize();
+
+    if (text.length <= 1) {
+      $('.alert').slideDown(1000).text("you gotta say something");
+      return;
+    }
+
+
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: text
-    });
+    }).then(loadTweets);
     console.log('text', text);
   });
 
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      dataType: 'JSON',
+      
+    }).then(renderTweets);
+    
+  };
 
-  renderTweets(data);
+  loadTweets();
 
 });
